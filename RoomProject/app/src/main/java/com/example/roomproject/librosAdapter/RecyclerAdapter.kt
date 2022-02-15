177ee2db1079a8fd3fc6e3bcc1da18242258b379
MiguelReid <mikeyreid2002@gmail.com>
2022-02-15 20:50:14 +0100

    Hecho el swipe y no estoy muy seguro de como rellenar la lista

--- RoomProject/app/src/main/java/com/example/roomproject/librosAdapter/RecyclerAdapter.kt
+++ RoomProject/app/src/main/java/com/example/roomproject/librosAdapter/RecyclerAdapter.kt
@@ -1,25 +1,36 @@
 package com.example.roomproject.librosAdapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomproject.R
+import com.example.roomproject.databinding.ActivityMainBinding
 import com.example.roomproject.model.LibrosDataClass
 
-class RecyclerAdapter (val arrayList: ArrayList<LibrosDataClass>): RecyclerView.Adapter<ViewHolder>() {
+class RecyclerAdapter (val lista: MutableList<LibrosDataClass>): RecyclerView.Adapter<ViewHolder>() {
+    //lo he cambiado lista a MutableList para q en el mainactivity lo pueda meter en el adapter
 
     override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): ViewHolder {
-        var inflater = LayoutInflater.from(parent.context).inflate(R.layout.lista_libros,parent,false)
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.lista_libros,parent,false)
         return ViewHolder(inflater)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val librs = arrayList[position]
-        holder.render(arrayList.get(position))
+
+        val libros = lista[position]
+        holder.render(lista.get(position))
+        //yo lo hago siempre diferente sin el render y en la misma clase
+        //si funciona dejarlo si pruebo la otra
     }
 
     override fun getItemCount(): Int {
-        return arrayList.size
+        return lista.size
     }
 
+    //para eliminar al deslizar
+    fun removeAt(position: Int){
+        lista.removeAt(position)
+        notifyItemRemoved(position)
+    }
 }
--- RoomProject/app/src/main/java/com/example/roomproject/view/MainActivity.kt
+++ RoomProject/app/src/main/java/com/example/roomproject/view/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.roomproject.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.roomproject.databinding.ActivityMainBinding
+import com.example.roomproject.librosAdapter.RecyclerAdapter
+import com.example.roomproject.model.LibrosDataBase
+import com.example.roomproject.model.LibrosDataClass
 import com.example.roomproject.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +20,54 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainrecycler: RecyclerView
     private lateinit var but: Button
 
+    //No estoy seguro de si se puede reutilizar la dataclass
+    lateinit var lista: MutableList<LibrosDataClass>
+    lateinit var adapter: RecyclerAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        lista = mutableListOf()
+        rellenarLayout()
+        swipe()
+    }
 
+    private fun rellenarLayout() {
+        val database = Room.databaseBuilder(
+            this, LibrosDataBase::class.java, "libros_database"
+        )
+            .allowMainThreadQueries()
+            .build()
 
+        database.libroDao.addLibro(Libros(titulo = "La Odisea", autor = "Homero"))
+        val inventario = database.libroDao.getLibros()
+
+        binding.recycler.layoutManager = LinearLayoutManager(this)
+        binding.recycler.setHasFixedSize(true)
+        adapter = RecyclerAdapter(inventario)
+        binding.recycler.adapter = adapter
+    }
+
+    private fun swipe() {
+        val itemTouch = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val adapter = binding.recycler.adapter as RecyclerAdapter
+                adapter.removeAt(viewHolder.adapterPosition)
+            }
+        }
+        val ith = ItemTouchHelper(itemTouch)
+        ith.attachToRecyclerView(binding.recycler)
     }
 }
